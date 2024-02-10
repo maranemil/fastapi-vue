914eb1096652648c746da3a64d070f77aa334aa4
EM <maran.emil@gmail.com>
2024-02-10 15:36:35 +0100

    update view names

--- services/frontend/src/views/NoteView.vue
+++ services/frontend/src/views/NoteView.vue
@@ -17,7 +17,7 @@ import { defineComponent } from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 
 export default defineComponent({
-  name: 'Note',
+  name: 'Note-view',
   props: ['id'],
   async created() {
     try {
--- services/frontend/src/views/ProfileView.vue
+++ services/frontend/src/views/ProfileView.vue
@@ -15,7 +15,7 @@ import { defineComponent } from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 
 export default defineComponent({
-  name: 'Profile',
+  name: 'Profile-view',
   created: function() {
     return this.$store.dispatch('viewMe');
   },
